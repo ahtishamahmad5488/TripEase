728c74b51d3c001f4ece44f94418b1f1f0002f75
Ahtisham Ahmad <ahtisham548849@gmail.com>
2025-02-22 11:57:19 +0500

    Create Folder Structure

---
+++ src/constants/screens.js
@@ -0,0 +1,11 @@
+import SplashScreen from '../screens/SplashScreen';
+import LoginScreen from '../screens/LoginScreen';
+import RegisterScreen from '../screens/RegisterScreen';
+import SignUpScreen from '../screens/SignUpScreen';
+
+export const SCREENS = {
+  LoginScreen: LoginScreen,
+  RegisterScreen: RegisterScreen,
+  SplashScreen: SplashScreen,
+  SignUpScreen: SignUpScreen,
+};
